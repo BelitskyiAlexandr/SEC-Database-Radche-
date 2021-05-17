8864f55b77ed617b0dd56aaa6c8c69f23e1e4514
BelitskyiAlexandr <vilisapett@gmail.com>
2021-05-18 01:14:54 +0300

    comments

--- lab 3/lab 3.py
+++ lab 3/lab 3.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 digits = datasets.load_digits()
 images_and_labels = list(zip(digits.images, digits.target))
@@ -8,16 +9,24 @@ for index, (image, label) in enumerate(images_and_labels[:8]):
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title('Train: %i' % label)
 
+# flatten the images
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
+
+# Create a classifier
 classifier = svm.SVC(gamma=0.001)
+
+# Learn the digits on the train subset
 classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
 expected = digits.target[n_samples // 2:]
+
+# Predict the value of the digit
 predicted = classifier.predict(data[n_samples // 2:])
 
 print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(expected, predicted)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
+#visualize the first 8 test samples and show their predicted digit value in the title
 images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
 for index, (image, prediction) in enumerate(images_and_predictions[:8]):
     plt.subplot(2, 8, index + 9)
